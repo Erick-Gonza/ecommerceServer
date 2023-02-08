9211a94712ff9fcd05ef352e1a7d3e2251a52c16
erickGzz <eagm080897@gmail.com>
2023-02-08 01:48:22 -0600

    Add dotenv import in server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import 'dotenv/config'
 import { router } from './routes/index.js'
 import { db } from './config/database.js'
 
-const port = process.env.PORT || 5000;
-const app = express();
+const port = process.env.PORT || 5000
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use(morgan('tiny'));
+app.use(morgan('tiny'))
 
 //Main route
-app.use('/api/v1', router);
+app.use('/api/v1', router)
 
-await db.sync({alter:true}).then(()=>{
+await db.sync({ alter: true }).then(() => {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}!`)
-  });
-});
-
+  })
+})
