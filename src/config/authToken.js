a21cf3f55a9cf136da0397ca44c4644668daafcd
erickGzz <eagm080897@gmail.com>
2023-02-27 22:46:17 -0600

    Fix token issue

--- src/config/authToken.js
+++ src/config/authToken.js
@@ -1,27 +1,18 @@
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 
 const secret = process.env.SECRET
 
-const generateToken = (payload) => {
-  return jwt.sign({ payload }, secret, { expiresIn: '3m' })
+const signToken = (user) => {
+  return jwt.sign(user, secret)
 }
 
-const validateToken = (token) => {
+const verifyToken = (token) => {
   return jwt.verify(token, secret)
 }
 
-const auth = (req, res, next) => {
-  const token = req.cookies.token
-  if (!token) {
-    return res.status(401).send({ message: 'Unauthorized: No token provided' })
-  }
-  try {
-    const verified = jwt.verify(token, secret)
-    req.user = verified
-    next()
-  } catch (error) {
-    res.status(400).send({ message: 'Invalid token' })
-  }
+const compareBcrypt = async (password, hash) => {
+  return await bcrypt.compare(password, hash)
 }
 
-export { generateToken, validateToken, auth }
+export { compareBcrypt, signToken, verifyToken }
