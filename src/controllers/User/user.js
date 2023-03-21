f3458ecb6323516f4b97395b0a26e93e33caffbb
Norma <nidaliaamtz@hotmail.com>
2023-03-20 20:01:51 -0600

    Nodemailer Signing up

--- src/controllers/User/user.js
+++ src/controllers/User/user.js
@@ -1,151 +1,152 @@
 // import { hashBcrypt } from '../../config/authToken.js'
 import { User, Address, Cart, WishList } from '../../models/index.js'
+import registerMail from '../../utils/mails/registerMail.js'
 
-const getAllUser = async (req, res) => {
-  try {
-    const data = await User.findAll({ include: [{ all: true }] })
-    data.length === 0
-      ? res.status(400).send({ message: 'No users found', success: false })
-      : res.status(200).send({ message: 'Get all users', success: true, data })
-  } catch (error) {
-    res.status(400).send({ message: error, success: false })
-  }
+const getAllUser = async(req, res) => {
+    try {
+        const data = await User.findAll({ include: [{ all: true }] })
+        data.length === 0 ?
+            res.status(400).send({ message: 'No users found', success: false }) :
+            res.status(200).send({ message: 'Get all users', success: true, data })
+    } catch (error) {
+        res.status(400).send({ message: error, success: false })
+    }
 }
 
-const getByIdUser = async (req, res) => {
-  try {
-    const { id } = req.params
-    const data = await User.findByPk(id, { include: [{ all: true }] })
+const getByIdUser = async(req, res) => {
+    try {
+        const { id } = req.params
+        const data = await User.findByPk(id, { include: [{ all: true }] })
 
-    data === null
-      ? res.status(400).send({
-        message: 'User with id ' + id + ' not found',
-        success: false
-      })
-      : res.status(200).send({
-        message: 'User with id ' + id + ' found',
-        success: true,
-        data,
-        userId: req.user.id
-      })
-  } catch (error) {
-    res.status(400).send({ message: error, success: false })
-  }
+        data === null ?
+            res.status(400).send({
+                message: 'User with id ' + id + ' not found',
+                success: false,
+            }) :
+            res.status(200).send({
+                message: 'User with id ' + id + ' found',
+                success: true,
+                data,
+                userId: req.user.id,
+            })
+    } catch (error) {
+        res.status(400).send({ message: error, success: false })
+    }
 }
 
-const createUser = async (req, res) => {
-  try {
-    const { firstName, lastName, userName, email, password, roleId, statusId } =
-      req.body
+const createUser = async(req, res) => {
+    try {
+        const { firstName, lastName, userName, email, password, roleId, statusId } =
+        req.body
 
-    const user = await User.create({
-      firstName,
-      lastName,
-      userName,
-      email,
-      password,
-      roleId,
-      statusId
-    })
-    const cart = await Cart.create({
-      userId: user.id
-    })
-    const wishlist = await WishList.create({
-      userId: user.id
-    })
-    const address = await Address.create({
-      street:'Change',
-      cityId:1,
-      zipCode:0,
-      countryId:1,
-      stateId:1,
-      userId: user.id
-    })
-    // TODO asignar el cartId y el wishlistId al usuario
-    res.send({
-      message: 'User created',
-      success: true,
-      user: {
-        cart,
-        wishlist,
-        address
-      }
-    })
-  } catch (error) {
-    res.status(400).send({ message: error, success: false })
-  }
+        const user = await User.create({
+            firstName,
+            lastName,
+            userName,
+            email,
+            password,
+            roleId,
+            statusId,
+        })
+        const cart = await Cart.create({
+            userId: user.id,
+        })
+        const wishlist = await WishList.create({
+            userId: user.id,
+        })
+        const address = await Address.create({
+            street: 'Change',
+            cityId: 1,
+            zipCode: 0,
+            countryId: 1,
+            stateId: 1,
+            userId: user.id,
+        })
+
+        await registerMail(firstName, email)
+
+        // TODO asignar el cartId y el wishlistId al usuario
+        res.send({
+            message: 'User created',
+            success: true,
+            user: {
+                cart,
+                wishlist,
+                address,
+            },
+        })
+    } catch (error) {
+        res.status(400).send({ message: error, success: false })
+    }
 }
 
-const updateUser = async (req, res) => {
-  try {
-    const { id } = req.params
-    const { firstName, lastName, userName, password, email, roleId, statusId } =
-      req.body
-    const user = await User.update(
-      {
-        firstName,
-        lastName,
-        userName,
-        email,
-        password,
-        roleId,
-        statusId
-      },
-      {
-        where: { id }
-      }
-    )
+const updateUser = async(req, res) => {
+    try {
+        const { id } = req.params
+        const { firstName, lastName, userName, password, email, roleId, statusId } =
+        req.body
+        const user = await User.update({
+            firstName,
+            lastName,
+            userName,
+            email,
+            password,
+            roleId,
+            statusId,
+        }, {
+            where: { id },
+        })
 
-    res.send({
-      message: 'User updated',
-      success: true,
-      user
-    })
-  } catch (error) {
-    res.status(400).send({ message: error, success: false })
-  }
+        res.send({
+            message: 'User updated',
+            success: true,
+            user,
+        })
+    } catch (error) {
+        res.status(400).send({ message: error, success: false })
+    }
 }
 
-const deleteUser = async (req, res) => {
-  try {
-    const { id } = req.params
-    await User.destroy({
-      where: {
-        id
-      }
-    })
-    res.send({ message: `User with ${id} deleted`, success: true })
-  } catch (error) {
-    res.status(400).send({ message: error, success: false })
-  }
+const deleteUser = async(req, res) => {
+    try {
+        const { id } = req.params
+        await User.destroy({
+            where: {
+                id,
+            },
+        })
+        res.send({ message: `User with ${id} deleted`, success: true })
+    } catch (error) {
+        res.status(400).send({ message: error, success: false })
+    }
 }
 
-const createUserAddress = async (req, res) => {
-  try {
-    const { id } = req.params
-    const { street, city, state, zipCode } = req.body
-    const address = await Address.create({
-      street,
-      city,
-      state,
-      zipCode,
-      UserId: id
-    })
-    res.send({
-      message: 'Address created',
-      success: true,
-      address
-    })
-  } catch (error) {
-    res.status(400).send({ message: error, success: false })
-  }
+const createUserAddress = async(req, res) => {
+    try {
+        const { id } = req.params
+        const { street, city, state, zipCode } = req.body
+        const address = await Address.create({
+            street,
+            city,
+            state,
+            zipCode,
+            UserId: id,
+        })
+        res.send({
+            message: 'Address created',
+            success: true,
+            address,
+        })
+    } catch (error) {
+        res.status(400).send({ message: error, success: false })
+    }
 }
 
 export {
-  getAllUser,
-  getByIdUser,
-  createUser,
-  updateUser,
-  deleteUser,
-  createUserAddress
+    getAllUser,
+    getByIdUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    createUserAddress,
 }
