9a11a9b0ff22bfc01a8f25f944a798bb45a3cbb4
erickGzz <eagm080897@gmail.com>
2023-03-23 17:51:46 -0600

    fix order detail controller

--- src/controllers/OrderDetail/orderDetail.js
+++ src/controllers/OrderDetail/orderDetail.js
@@ -20,35 +20,22 @@ const getOrderDetailByUserId = async (req, res) => {
 }
 
 const createOrderDetail = async (req, res) => {
-  const { quantity, productId, orderId } = req.body
-  const orderItem = await OrderDetail.create({
-    quantity,
-    orderId,
-    productId
-  })
+  try {
+    const { quantity, productId, orderId } = req.body
+    const orderItem = await OrderDetail.create({
+      quantity,
+      orderId,
+      productId
+    })
 
-  res.status(201).send({
-    message: 'order item added',
-    success: true,
-    orderItem
-  })
-  // try {
-  //   const { quantity, productId, orderId } = req.body
-  //   const order = await OrderDetail.findByPk(orderId)
-  //   const orderItem = await OrderDetail.create({
-  //     quantity,
-  //     orderId: order.id,
-  //     productId
-  //   })
-
-  //   res.status(201).send({
-  //     message: 'order item added',
-  //     success: true,
-  //     orderItem
-  //   })
-  // } catch (error) {
-  //   res.status(400).send({ message: error, success: false })
-  // }
+    res.status(201).send({
+      message: 'order item added',
+      success: true,
+      orderItem
+    })
+  } catch (error) {
+    res.status(400).send({ message: error, success: false })
+  }
 }
 
 export {
